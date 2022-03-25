71222a3ef8e3ef605d44c1a7d7144fdbfc127ab7
composer6251 <composercoder57>
2022-03-25 09:20:33 -0400

    Wiring up FeignClient

--- src/main/java/com/recordcataloguer/recordcataloguer/client/EbaySandboxClient.java
+++ src/main/java/com/recordcataloguer/recordcataloguer/client/EbaySandboxClient.java
@@ -2,16 +2,13 @@ package com.recordcataloguer.recordcataloguer.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.Map;
 
-@Service
-@FeignClient(value = EbayUrls.SANDBOX_AUTHORIZATION_API_PATH, url = EbayUrls.EBAY_SANDBOX_BASE_URL)
+@FeignClient(name="feign-client", url = EbayUrls.EBAY_SANDBOX_BASE_URL)
 public interface EbaySandboxClient {
 
     //todo: Configure ebay request
--- src/main/java/com/recordcataloguer/recordcataloguer/service/EbayService.java
+++ src/main/java/com/recordcataloguer/recordcataloguer/service/EbayService.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @Service
 @Slf4j
 public class EbayService {
+    @Autowired
     private EbaySandboxClient ebayClient;
 
     private Map<String, String> headers = new HashMap<>();
